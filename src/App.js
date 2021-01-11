9b12afec5727d7f8cf2b8d05b10efeff01678a7f
Axel Thiel <axth2220@gmail.com>
2021-01-11 13:37:05 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { LoginPage } from "./pages/LoginPage";
+import { CustomerDetailPage } from "./pages/CustomerDetailPage";
+import { CustomerListPage } from "./pages/CustomerListPage";
 
 function App() {
-  return <div>hej</div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/customers">
+          <CustomerListPage />
+        </Route>
+        <Route path="/customers/:id">
+          <CustomerDetailPage />
+        </Route>
+        <Route path="/">
+          <LoginPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
