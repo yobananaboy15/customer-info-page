58ef6f928eb49b96872069ba4edd3740541a68d4
Axel Thiel <axth2220@gmail.com>
2021-01-17 12:06:15 +0100

    Add styled button components

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  color: #fff;
+  background: #286090;
+  border: none;
+  padding: 0.4rem 1.5rem;
+  border-radius: 0.25rem;
+
+  &:hover {
+    filter: brightness(120%);
+  }
+`;
---
+++ src/components/ButtonDelete.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import { Button } from "./Button";
+
+export const ButtonDelete = styled(Button)`
+  background: #c9302c;
+`;
